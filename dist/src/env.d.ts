98d92f90516ff21d1748ab5ad38ab6f92224cd26
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-12-17 19:55:26 +0100

    skip ci

--- dist/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const env: {
-    SECRET: string;
-    WITH_DECRYPTION: boolean;
-    PARAMETER_PREFIX: string | undefined;
-    ENV_FILE_PATH: string;
-    IS_JSON: boolean;
-};
-//# sourceMappingURL=env.d.ts.map
--- dist/src/schemas/json.d.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-type Literal = z.infer<typeof literalSchema>;
-type Json = Literal | {
-    [key: string]: Json;
-} | Json[];
-declare const literalSchema: z.ZodUnion<[z.ZodString, z.ZodNumber, z.ZodBoolean, z.ZodNull]>;
-export declare const jsonSchema: z.ZodType<Json>;
-export {};
-//# sourceMappingURL=json.d.ts.map
