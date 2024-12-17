e20d0f7ca19b826138212621a1715d4e0028030c
Emmanuel Isenah <86387828+Armadillidiid@users.noreply.github.com>
2024-12-17 20:02:30 +0100

    Development (#2)
    * feat: update ci
    
    * refactor: rename action file extension
    
    * skip ci

--- dist/src/constant.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const MAX_CONCURRENT_SSM_PROMISES = 3;
-export declare const ENV_FILENAME = ".env";
-export declare const ENV_OUTPUT_KEY = "env";
-//# sourceMappingURL=constant.d.ts.map
